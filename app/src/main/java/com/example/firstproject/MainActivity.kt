e1d190597132a08728085aede7197aa350c1afda
johnathan <momo.johnathan@gmail.com>
2023-05-24 14:41:19 +0800

    test star rotate animation

--- app/src/main/java/com/example/firstproject/MainActivity.kt
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -4,32 +4,56 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationState
+import androidx.compose.animation.core.EaseInBack
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.firstproject.ui.theme.FirstProjectTheme
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,12 +83,14 @@ fun MessageCard(message: Message) {
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
+            val interactionSource = MutableInteractionSource()
             Image(
                 painter = painterResource(id = R.drawable.baseline_arrow_back_ios_new_24),
                 contentDescription = "test back icon",
-                Modifier.clickable {
+                Modifier.clickable(interactionSource = interactionSource, indication = null) {
                     Log.d("J", "right")
-                    starColor = Color.Red.value
+//                    starColor = Color.Red.value
+
                 }
             )
             Text(text = message.name)
@@ -77,8 +103,10 @@ fun MessageCard(message: Message) {
                 }
             )
         }
-        Row {
-            Text(text = message.value)
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            colorBox(Color.Blue)
+            colorBox(Color.Yellow)
+            colorBox(Color.Red)
         }
     }
 
@@ -92,14 +120,45 @@ fun PreviewMessageCard() {
 
 @Composable
 fun StarComponent(starState: Boolean, onStarStateChanged: (Boolean) -> Unit) {
+
+    val interactionSource = MutableInteractionSource()
+    val coroutineScope = rememberCoroutineScope()
+    var currentRotation by remember { mutableStateOf(0f) }
+    val rotation = remember { Animatable(currentRotation) }
+
     Image(
         painter = painterResource(id = if (starState) R.drawable.icon_start_hollow_24x24 else R.drawable.icon_start_full_24x24),
         contentDescription = "icon starts",
-//        colorFilter = ColorFilter.tint(color),
         modifier = Modifier
             .size(80.dp)
-            .clickable {
+            .rotate(currentRotation)
+            .clickable(interactionSource = interactionSource, indication = null) {
                 onStarStateChanged.invoke(starState.not())
+                coroutineScope.launch {
+                    rotation.animateTo(
+                        currentRotation + 30f,
+                        tween(durationMillis = 1000, easing = LinearOutSlowInEasing)
+                    ) {
+                        currentRotation = value
+                    }
+                }
             },
     )
+}
+
+@Composable
+fun colorBox(color: Color) {
+    val interactionSource = MutableInteractionSource()
+    val coroutineScope = rememberCoroutineScope()
+    val test = remember { Animatable(color) }
+
+    Box(
+        Modifier
+            .size(24.dp)
+            .background(color = test.value)
+            .clickable(interactionSource = interactionSource, indication = null) {
+                coroutineScope.launch {
+                    test.animateTo(Color.Black, animationSpec = tween(1000))
+                }
+            })
 }
